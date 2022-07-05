5878431d7761797d08d110716a46e12f5e2f25b3
Cristian Ledesma <cr.ledesma@hotmail.com>
2022-07-05 14:57:03 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { TaskCreator } from "./components/TaskCreator";
+import TaskTable from './components/TaskTable';
+import VisibilityControl from './components/VisibilityControl';
 
 function App() {
+  const [tasksItems, setTasksItems] = useState([]);
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  const createNewTask = (taskName) => {
+    if (!tasksItems.find(task => task.name === taskName)) {
+      setTasksItems([ ...tasksItems, { name: taskName, done: false}]);
+    }
+  };
+
+  const toggleTask = (task) => {
+    setTasksItems(
+      tasksItems.map((t) => (t.name === task.name ? { ...t, done: !t.done } : t))
+    );
+  }
+
+  const cleanTasks = () => {
+    setTasksItems(tasksItems.filter(task => !task.done));
+    setShowCompleted(false);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasksItems));
+  }, [ tasksItems ])
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasksItems));
+  }, [ tasksItems ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-dark vh-100 text-white">
+      <div className="container p-4 col-md-4 offset-md-4">
+        <TaskCreator createNewTask={createNewTask} />
+        <TaskTable tasks={tasksItems} toggleTask={toggleTask} />
+        <VisibilityControl
+        isChecked={showCompleted}
+        showCompleted={(showCompleted)}
+        setShowCompleted={setShowCompleted} 
+        cleanTasks={cleanTasks}
+        />
+        {
+          showCompleted && (
+            <TaskTable tasks={tasksItems} toggleTask={toggleTask} showCompleted={showCompleted} />
+          )
+        }
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/VisibilityControl.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function VisibilityControl({ isChecked, setShowCompleted, cleanTasks }) {
+  const handleDelete = () => {
+    if (window.confirm('Are you sure you want to delete all completed tasks?')) {
+      cleanTasks()
+    }
+};
+
+return (
+    <div className="d-flex justify-content-between bg-secondary text-white text-center p-2 border-secondary">
+      <div className="form-check form-switch">
+        <input
+            className="form-check-input"
+            type="checkbox"
+            checked={isChecked}
+            onChange={(e) => setShowCompleted(e.target.checked)}
+        />{" "}
+        <label>Show tasks done</label>
+      </div>
+      <button className="btn btn-danger btn-sm"onClick={handleDelete}>Cleaer</button>
+    </div>
+  );
+}
